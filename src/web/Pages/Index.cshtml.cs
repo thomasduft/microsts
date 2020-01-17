29693e9dba301c8e30d2b0f9e8d54b4094c3d077
thomasduft <thomas.duft@gmail.com>
2020-01-17 13:49:11 +0100

    syntax sugar

--- src/web/Pages/Index.cshtml.cs
+++ src/web/Pages/Index.cshtml.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace tomware.Microsts.Web.Pages
 {
-    public class IndexModel : PageModel
+  public class IndexModel : PageModel
+  {
+    public void OnGet()
     {
-        public void OnGet()
-        {
-        }
     }
+  }
 }
