79e54d14d71e3ab31856b61488f6013c022b9340
Thomas Duft <thomas.duft@gmail.com>
2020-02-16 17:20:59 +0100

    support for linux-arm docker container (wip)

--- deploy.sh
+++ deploy.sh
@@ -18,14 +18,16 @@ echo Restore solution
 dotnet restore src/web/tomware.Microsts.Web.csproj
 
 echo Building solution
-dotnet publish src/web/tomware.Microsts.Web.csproj -c Release -o ./publish/
+dotnet publish src/web/tomware.Microsts.Web.csproj -c Release -r linux-x64 -o ./publish/linux-x64
+# dotnet publish src/web/tomware.Microsts.Web.csproj -c Release -r linux-arm -o ./publish/linux-arm 
 
 # echo Deploy web app
 # cd src/clientapp && npm i && npm run build-webclient && cd ../..;
 
 echo Building docker image tomware/microsts:$1
 #echo $PWD
-docker build --build-arg source=publish -t tomware/microsts:$1 .
+docker build --build-arg source=publish/linux-x64 -t tomware/microsts:$1 .
+# docker build --build-arg source=publish/linux-arm -t tomware/microsts:$1-linux-arm .
 
 #echo Cleaning up
 #if [ -d "publish" ]
