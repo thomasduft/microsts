725fe9c1db5661ac5b93221c22a28ca7f7e0d869
thomasduft <thomas.duft@gmail.com>
2020-02-21 10:03:22 +0100

    removed AuthInterceptor

--- src/clientapp/src/app/shared/services/interceptors.ts
+++ src/clientapp/src/app/shared/services/interceptors.ts
@@ -21,24 +21,6 @@ import { StatusMessage, StatusLevel } from './models';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthInterceptor implements HttpInterceptor {
-  public constructor(
-    private oauthService: OAuthService
-  ) { }
-
-  public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.oauthService.getAccessToken();
-    if (token) {
-      req = req.clone({ headers: req.headers.set('Authorization', 'Bearer ' + token) });
-    }
-
-    return next.handle(req);
-  }
-}
-
-@Injectable({
-  providedIn: 'root'
-})
 export class HttpErrorInterceptor implements HttpInterceptor {
   public constructor(
     private router: Router,
@@ -53,9 +35,6 @@ export class HttpErrorInterceptor implements HttpInterceptor {
         }
       }, (err: any) => {
         if (err instanceof HttpErrorResponse) {
-          if (err.status === 401 || err.status === 0) {
-            this.router.navigate(['login']);
-          }
           if (err.status === 403) {
             this.router.navigate(['forbidden']);
           }
@@ -98,6 +77,5 @@ export class AuthGuard implements CanActivate, CanActivateChild {
 }
 
 export const httpInterceptorProviders = [
-  // { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true }
 ];
