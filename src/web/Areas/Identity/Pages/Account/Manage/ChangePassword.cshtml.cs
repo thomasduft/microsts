673567c8b7e0547c0880c3abaa2f832687701157
thomasduft <thomas.duft@gmail.com>
2020-05-01 12:14:13 +0200

    localization manage (WIP)

--- src/web/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ src/web/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using tomware.Microsts.Web.Resources;
 
 namespace tomware.Microsts.Web.Areas.Identity.Pages.Account.Manage
 {
@@ -12,15 +13,19 @@ namespace tomware.Microsts.Web.Areas.Identity.Pages.Account.Manage
     private readonly UserManager<ApplicationUser> userManager;
     private readonly SignInManager<ApplicationUser> signInManager;
     private readonly ILogger<ChangePasswordModel> logger;
+    private readonly IdentityLocalizationService identityLocalizationService;
 
     public ChangePasswordModel(
-        UserManager<ApplicationUser> userManager,
-        SignInManager<ApplicationUser> signInManager,
-        ILogger<ChangePasswordModel> logger)
+      UserManager<ApplicationUser> userManager,
+      SignInManager<ApplicationUser> signInManager,
+      ILogger<ChangePasswordModel> logger,
+      IdentityLocalizationService identityLocalizationService
+    )
     {
       this.userManager = userManager;
       this.signInManager = signInManager;
       this.logger = logger;
+      this.identityLocalizationService = identityLocalizationService;
     }
 
     [BindProperty]
@@ -31,20 +36,24 @@ namespace tomware.Microsts.Web.Areas.Identity.Pages.Account.Manage
 
     public class InputModel
     {
-      [Required]
+      [Required(ErrorMessage = "CURRENT_PASSWORD_REQUIRED")]
       [DataType(DataType.Password)]
-      [Display(Name = "Current password")]
       public string OldPassword { get; set; }
 
-      [Required]
-      [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+      [Required(ErrorMessage = "NEW_PASSWORD_REQUIRED")]
+      [StringLength(
+        100,
+        ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
+        MinimumLength = 6
+      )]
       [DataType(DataType.Password)]
-      [Display(Name = "New password")]
       public string NewPassword { get; set; }
 
       [DataType(DataType.Password)]
-      [Display(Name = "Confirm new password")]
-      [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+      [Compare(
+        "NewPassword",
+        ErrorMessage = "CONFIRM_PASSWORD_NOT_MATCHING"
+      )]
       public string ConfirmPassword { get; set; }
     }
 
@@ -94,8 +103,11 @@ namespace tomware.Microsts.Web.Areas.Identity.Pages.Account.Manage
       }
 
       await signInManager.RefreshSignInAsync(user);
+
       logger.LogInformation("User changed their password successfully.");
-      StatusMessage = "Your password has been changed.";
+
+      StatusMessage = this.identityLocalizationService
+        .GetLocalizedHtmlString("CHANGE_PASSWORD_STATUS");
 
       return RedirectToPage();
     }
--- src/web/Areas/Identity/Pages/Account/Manage/ExternalLogins.cshtml.cs
+++ src/web/Areas/Identity/Pages/Account/Manage/ExternalLogins.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using tomware.Microsts.Web.Resources;
 
 namespace tomware.Microsts.Web.Areas.Identity.Pages.Account.Manage
 {
@@ -13,13 +14,17 @@ namespace tomware.Microsts.Web.Areas.Identity.Pages.Account.Manage
   {
     private readonly UserManager<ApplicationUser> userManager;
     private readonly SignInManager<ApplicationUser> signInManager;
+    private readonly IdentityLocalizationService identityLocalizationService;
 
     public ExternalLoginsModel(
-        UserManager<ApplicationUser> userManager,
-        SignInManager<ApplicationUser> signInManager)
+      UserManager<ApplicationUser> userManager,
+      SignInManager<ApplicationUser> signInManager,
+      IdentityLocalizationService identityLocalizationService
+    )
     {
       this.userManager = userManager;
       this.signInManager = signInManager;
+      this.identityLocalizationService = identityLocalizationService;
     }
 
     public IList<UserLoginInfo> CurrentLogins { get; set; }
@@ -36,37 +41,46 @@ namespace tomware.Microsts.Web.Areas.Identity.Pages.Account.Manage
       var user = await userManager.GetUserAsync(User);
       if (user == null)
       {
-        return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+        return NotFound(this.identityLocalizationService
+         .GetLocalizedHtmlString("USER_NOTFOUND", userManager.GetUserId(User)));
       }
 
       CurrentLogins = await userManager.GetLoginsAsync(user);
       OtherLogins = (await signInManager.GetExternalAuthenticationSchemesAsync())
           .Where(auth => CurrentLogins.All(ul => auth.Name != ul.LoginProvider))
           .ToList();
+
       ShowRemoveButton = user.PasswordHash != null || CurrentLogins.Count > 1;
+
       return Page();
     }
 
     public async Task<IActionResult> OnPostRemoveLoginAsync(
-        string loginProvider,
-        string providerKey
+      string loginProvider,
+      string providerKey
     )
     {
       var user = await userManager.GetUserAsync(User);
       if (user == null)
       {
-        return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+        return NotFound(this.identityLocalizationService
+         .GetLocalizedHtmlString("USER_NOTFOUND", userManager.GetUserId(User)));
       }
 
       var result = await userManager.RemoveLoginAsync(user, loginProvider, providerKey);
       if (!result.Succeeded)
       {
-        StatusMessage = "The external login was not removed.";
+        StatusMessage = this.identityLocalizationService
+          .GetLocalizedHtmlString("STATUS_EXTERNAL_LOGIN_REMOVED");
+
         return RedirectToPage();
       }
 
       await signInManager.RefreshSignInAsync(user);
-      StatusMessage = "The external login was removed.";
+
+      StatusMessage = this.identityLocalizationService
+          .GetLocalizedHtmlString("STATUS_EXTERNAL_LOGIN_REMOVED");
+
       return RedirectToPage();
     }
 
@@ -91,10 +105,12 @@ namespace tomware.Microsts.Web.Areas.Identity.Pages.Account.Manage
       var user = await userManager.GetUserAsync(User);
       if (user == null)
       {
-        return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+        return NotFound(this.identityLocalizationService
+          .GetLocalizedHtmlString("USER_NOTFOUND", userManager.GetUserId(User)));
       }
 
-      var info = await signInManager.GetExternalLoginInfoAsync(await userManager.GetUserIdAsync(user));
+      var info = await signInManager
+        .GetExternalLoginInfoAsync(await userManager.GetUserIdAsync(user));
       if (info == null)
       {
         throw new InvalidOperationException($"Unexpected error occurred loading external login info for user with ID '{user.Id}'.");
@@ -103,14 +119,14 @@ namespace tomware.Microsts.Web.Areas.Identity.Pages.Account.Manage
       var result = await userManager.AddLoginAsync(user, info);
       if (!result.Succeeded)
       {
-        StatusMessage = "The external login was not added. External logins can only be associated with one account.";
-        return RedirectToPage();
+        throw new InvalidOperationException($"Unexpected error occurred adding external login for user with ID '{user.Id}'.");
       }
 
       // Clear the existing external cookie to ensure a clean login process
       await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
-      StatusMessage = "The external login was added.";
+      StatusMessage = this.identityLocalizationService
+                        .GetLocalizedHtmlString("STATUS_EXTERNAL_LOGIN_ADDED");
 
       return RedirectToPage();
     }
