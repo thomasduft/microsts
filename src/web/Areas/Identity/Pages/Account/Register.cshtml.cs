aa371336818d95c15dc3e5af0f375e0e881bd693
thomasduft <thomas.duft@gmail.com>
2020-05-12 09:19:29 +0200

    removed unused using statmentes

--- src/web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
